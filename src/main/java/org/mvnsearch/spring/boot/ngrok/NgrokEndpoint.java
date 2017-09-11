426e415211138f918fe29337203b23dafffc3292
linux_china <linux_china@hotmail.com>
2017-09-11 12:36:07 +0800

    ngrok endpoint

---
+++ src/main/java/org/mvnsearch/spring/boot/ngrok/NgrokEndpoint.java
@@ -0,0 +1,24 @@
+package org.mvnsearch.spring.boot.ngrok;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * ngrok endpoint
+ *
+ * @author linux_china
+ */
+@ConfigurationProperties(prefix = "endpoints.hessian", ignoreUnknownFields = false)
+public class NgrokEndpoint extends AbstractEndpoint<TunnelDetail> {
+    @Autowired
+    private NgrokTunnel tunnel;
+
+    public NgrokEndpoint() {
+        super("ngrok");
+    }
+
+    public TunnelDetail invoke() {
+        return tunnel.getDetail();
+    }
+}
